88dfe9ed06622c816f0a0bb3c5e683804cdc21fa
Bilal Ashraf <bilalashraf6233@gmail.com>
2024-11-19 16:22:01 +0500

    google tts is done

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home"; // Your home page component
 import "./App.css";
+import GoogleTTS from "./pages/GoogleTTS";
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
       <Routes>
         {/* Home Page Route */}
         <Route path="/" element={<Home />} />
+        <Route path="/tts" element={<GoogleTTS />} />
       </Routes>
     </Router>
   );
